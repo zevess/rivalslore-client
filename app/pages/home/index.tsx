cc08338dd2ee42910f2e72cb664fc46508acc4f4
zevess <gazetdinovvlad04@gmail.com>
2025-10-07 18:23:53 +0300

    added favicon, home image fixes

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -8,12 +8,12 @@ export const HomePage: React.FC<Props> = ({ className }) => {
     return (
 
         <div className="flex flex-col items-center gap-4 h-full w-full">
-            <img src="app/shared/assets/bg.jpg" className="mask-x-from-80% mask-x-to-100% mask-y-from-90% mask-y-to-100%  bg-no-repeat" alt="" />
+            <img src="/bg.jpg" className="mask-x-from-80% mask-x-to-100% mask-y-from-90% mask-y-to-100%  bg-no-repeat" alt="" />
 
             <div className="flex flex-col items-center gap-4">
                 <h1 className="font-extrabold text-4xl md:text-7xl text-center text-white">Добро пожаловать <br /> в RIVALSLORE</h1>
                 <h2 className="font-medium text-4xl text-center text-white">- сайт с официальным лором игры</h2>
-                <img src="app/shared/assets/rivals-logo.png" className="w-full max-w-[800px]" />
+                <img src="/rivals-logo.png" className="w-full max-w-[800px]" />
             </div>
 
         </div>
